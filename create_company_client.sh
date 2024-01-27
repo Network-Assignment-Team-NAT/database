f19e0b0b5fb7d43d4dec49f1982865d75ef3a8f6
root <root@random-bullshit-go-com.novalocal>
2024-01-27 12:33:18 +0000

    Fix company client

--- create_company_client.sh
+++ create_company_client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-psql -U postgres -d nat_database -c "INSERT INTO company_clients (company_name, tariff, enable_until_date) VALUES ('$1', '$2', '$3');"
+psql -U postgres -d nat_database -c "INSERT INTO company_clients (company_name, tariff, service_until) VALUES ('$1', $2, '$3');"
