e842c6e2251c46a1c01705c9e3477192eefd089b
root <root@random-bullshit-go-com.novalocal>
2024-01-27 12:44:13 +0000

    Fix company add again

--- create_company_client.sh
+++ create_company_client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-psql -U postgres -d nat_database -c "INSERT INTO company_clients (company_name, tariff, service_until) VALUES ('$1', $2, '$3');"
+psql -U postgres -d nat_database -c "INSERT INTO company_clients (name, tariff, service_until) VALUES ('$1', $2, '$3');"
